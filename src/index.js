919e248ce2f22219f7fb8b0c4e8dcbe19b7c9045
Gosse Yannick Cyriaque GB <gosseyannick553@gmail.com>
2024-11-26 21:54:51 +0000

    feat: installing and configuring express session for user sessions management

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
 
 const { router: recipesRouter } = require("./router/recipes");
 const { router: productsRouter } = require("./router/products");
@@ -7,6 +8,16 @@ const { router: productsRouter } = require("./router/products");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "teddy smith dev",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 60000 * 60,
+    },
+  })
+);
 
 const publicDirectoryPath = path.join(__dirname, "./public");
 app.use(express.static(publicDirectoryPath));
